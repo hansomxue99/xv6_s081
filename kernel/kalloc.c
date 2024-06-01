ac6d9f1c93154f48f639955f1d7ea9f22da92683
hansomxue99 <404245963@qq.com>
2024-06-01 17:36:11 +0800

    lab6-cow

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -9,6 +9,10 @@
 #include "riscv.h"
 #include "defs.h"
 
+#define PA2INDEX(pa) ((uint64)pa / PGSIZE)
+
+int cowcount[PHYSTOP / PGSIZE];
+
 void freerange(void *pa_start, void *pa_end);
 
 extern char end[]; // first address after kernel.
@@ -35,8 +39,10 @@ freerange(void *pa_start, void *pa_end)
 {
   char *p;
   p = (char*)PGROUNDUP((uint64)pa_start);
-  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
+  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE) {
+    cowcount[PA2INDEX(p)] = 1;
     kfree(p);
+  }
 }
 
 // Free the page of physical memory pointed at by v,
@@ -50,6 +56,14 @@ kfree(void *pa)
 
   if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
     panic("kfree");
+
+  acquire(&kmem.lock);
+  int remain = --cowcount[PA2INDEX(pa)];
+  release(&kmem.lock);
+
+  if (remain > 0) {
+    return;
+  }
 
   // Fill with junk to catch dangling refs.
   memset(pa, 1, PGSIZE);
@@ -76,7 +90,22 @@ kalloc(void)
     kmem.freelist = r->next;
   release(&kmem.lock);
 
-  if(r)
+  if(r) {
     memset((char*)r, 5, PGSIZE); // fill with junk
+    int idx = PA2INDEX(r);
+    if (cowcount[idx] != 0) {
+      panic("kalloc: cowcount[idx] != 0");
+    }
+    cowcount[idx] = 1;
+  }
   return (void*)r;
+}
+
+void adjustref(uint64 pa, int num) {
+  if (pa > PHYSTOP) {
+    panic("addref: pa too big");
+  }
+  acquire(&kmem.lock);
+  cowcount[PA2INDEX(pa)] += num;
+  release(&kmem.lock);
 }
--- kernel/vm.c
+++ kernel/vm.c
@@ -311,7 +311,7 @@ uvmcopy(pagetable_t old, pagetable_t new, uint64 sz)
   pte_t *pte;
   uint64 pa, i;
   uint flags;
-  char *mem;
+  // char *mem;
 
   for(i = 0; i < sz; i += PGSIZE){
     if((pte = walk(old, i, 0)) == 0)
@@ -319,20 +319,48 @@ uvmcopy(pagetable_t old, pagetable_t new, uint64 sz)
     if((*pte & PTE_V) == 0)
       panic("uvmcopy: page not present");
     pa = PTE2PA(*pte);
+    *pte &= ~PTE_W;
     flags = PTE_FLAGS(*pte);
-    if((mem = kalloc()) == 0)
-      goto err;
-    memmove(mem, (char*)pa, PGSIZE);
-    if(mappages(new, i, PGSIZE, (uint64)mem, flags) != 0){
-      kfree(mem);
+    // if((mem = kalloc()) == 0)
+    //   goto err;
+    // memmove(mem, (char*)pa, PGSIZE);
+    if(mappages(new, i, PGSIZE, pa, flags) != 0){
+      // kfree(mem);
       goto err;
     }
+    adjustref(pa, 1);
   }
   return 0;
 
  err:
   uvmunmap(new, 0, i / PGSIZE, 1);
   return -1;
+}
+
+int cowalloc(pagetable_t pagetable, uint64 va) {
+  if (va >= MAXVA) {
+    printf("cowalloc: exceeds MAXVA\n");
+    return -1;
+  }
+  pte_t *pte = walk(pagetable, va, 0);
+  if (pte == 0) {
+    panic("cowalloc: pte not exists");
+  }
+
+  if ((*pte & PTE_V) == 0 || (*pte & PTE_U) == 0) {
+    panic("cowalloc: pte permission err");
+  }
+
+  uint64 pa_new = (uint64)kalloc();
+  if (pa_new == 0) {
+    printf("cowalloc: kalloc fails\n");
+    return -1;
+  }
+  uint64 pa_old = PTE2PA(*pte);
+  memmove((void *)pa_new, (const void *)pa_old, PGSIZE);
+  kfree((void *)pa_old);
+  *pte = PA2PTE(pa_new) | PTE_FLAGS(*pte) | PTE_W;
+  return 0;
 }
 
 // mark a PTE invalid for user access.
@@ -358,7 +386,23 @@ copyout(pagetable_t pagetable, uint64 dstva, char *src, uint64 len)
 
   while(len > 0){
     va0 = PGROUNDDOWN(dstva);
-    pa0 = walkaddr(pagetable, va0);
+    if (va0 >= MAXVA) {
+      printf("copyout: va exceeds MAXVA\n");
+      return -1;
+    }
+    pte_t *pte = walk(pagetable, va0, 0);
+    if (pte == 0 || (*pte & PTE_U) == 0 || (*pte & PTE_V) == 0) {
+      printf("copyout: invalid pte\n");
+      return -1;
+    }
+    if ((*pte & PTE_W) == 0) {
+      // 写的目的地是COW共享页, 需要复制一份
+      if (cowalloc(pagetable, va0) < 0) {
+        return -1;
+      }
+    }
+    // pa0 = walkaddr(pagetable, va0);
+    pa0 = PTE2PA(*pte);
     if(pa0 == 0)
       return -1;
     n = PGSIZE - (dstva - va0);
