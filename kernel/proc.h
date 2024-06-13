31d914fe9b935daa1499a231fd517ce9d27336ca
hansomxue99 <404245963@qq.com>
2024-06-13 18:02:37 +0800

    lab10-mmap

--- kernel/proc.h
+++ kernel/proc.h
@@ -82,6 +82,21 @@ struct trapframe {
 
 enum procstate { UNUSED, SLEEPING, RUNNABLE, RUNNING, ZOMBIE };
 
+// Number of virtual memory area per process
+#define NVMA 16
+
+// Virtual Memory Area
+struct vma {
+ int valid;
+ uint64 addr;
+ int length;
+ int prot;
+ int flags;
+ int fd;
+ int offset;
+ struct file* f;
+};
+
 // Per-process state
 struct proc {
   struct spinlock lock;
@@ -103,4 +118,5 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  struct vma vmas[NVMA];
 };
